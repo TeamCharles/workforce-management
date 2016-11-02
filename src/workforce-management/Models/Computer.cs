17095354775d16ec6be10da7ff9fbb2661cc2cfc
Matt Kraatz <Matt Kraatz>
2016-11-02 13:15:12 -0700

    boilerplate vs solution

---
+++ src/workforce-management/Models/Computer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Bangazon.Models
+{
+    public class Computer
+    {
+        /**
+         * Class: Computer
+         * Purpose: Represents the Computer table in the database
+         * Author: Matt Kraatz
+         */
+        [Key]
+        public int ComputerId { get; set; }
+        [Required]
+        [DataType(DataType.Date)]
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        public DateTime DateCreated { get; set; }
+
+        [Required]
+        public string Make { get; set; }
+
+        [Required]
+        public string Model { get; set; }
+
+        [Required]
+        [Display(Name = "Serial Number")]
+        public string SerialNumber { get; set; }
+
+        // Foreign Key Dependencies
+        public ICollection<Employee> Employees;
+    }
+}
