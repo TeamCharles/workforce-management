cf212f7b1da0f25f89869782e549a030ba45da68
Garrett Vangilder <garrett.vangilder@gmail.com>
2016-11-03 18:39:38 -0500

    init commit

---
+++ src/workforce-management/Controllers/TrainingProgramController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace workforce_management.Controllers
+{
+    public class TrainingProgramController
+    {
+    }
+}
