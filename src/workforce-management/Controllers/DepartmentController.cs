135bced0aee475f9f6049329f4bac0f66b05ff77
Dayne Wright <daynewr@gmail.com>
2016-11-03 13:48:22 -0500

    added department controller/viewmodel/view

---
+++ src/workforce-management/Controllers/DepartmentController.cs
@@ -0,0 +1,29 @@
+﻿using BangazonWeb.Data;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using workforce_management.ViewModels;
+
+namespace workforce_management.Controllers
+{
+    public class DepartmentController : Controller
+    {
+        private BangazonContext context;
+
+        public DepartmentController(BangazonContext ctx)
+        {
+            context = ctx;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var model = new DepartmentIndex();
+            model.DepartmentList = from department in context.Department select department; 
+
+            return View(model);
+        }
+    }
+}
