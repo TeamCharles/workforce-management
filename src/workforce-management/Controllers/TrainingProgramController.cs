0746f28001da756b22f449d1937d0a0c919eb6f0
Matt Kraatz <Matt Kraatz>
2016-11-03 09:25:21 -0700

    wrote controller, viewmodel and view for TrainingPrograms/Index state

---
+++ src/workforce-management/Controllers/TrainingProgramController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BangazonWeb.Data;
+using Microsoft.AspNetCore.Mvc;
+using workforce_management.ViewModels;
+
+namespace workforce_management.Controllers
+{
+    public class TrainingProgramController: Controller
+    {
+
+        private BangazonContext context;
+
+        public TrainingProgramController(BangazonContext ctx)
+        {
+            context = ctx;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var model = new TrainingProgramIndex();
+            model.TrainingPrograms = from program in context.TrainingProgram select program;
+            return View(model);
+        }
+    }
+}
---
+++ src/workforce-management/ViewModels/TrainingProgram/TrainingProgramIndex.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Bangazon.Models;
+
+namespace workforce_management.ViewModels
+{
+    public class TrainingProgramIndex
+    {
+        public IEnumerable<TrainingProgram> TrainingPrograms { get; set; }
+    }
+}
