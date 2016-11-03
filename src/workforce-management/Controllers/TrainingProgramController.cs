1b2fbed5dada969fa0e48b9dca2a63d56a7c4424
Matt Kraatz <Matt Kraatz>
2016-11-03 09:33:22 -0700

    added documentation

--- src/workforce-management/Controllers/TrainingProgramController.cs
+++ src/workforce-management/Controllers/TrainingProgramController.cs
@@ -10,14 +10,32 @@ namespace workforce_management.Controllers
 {
     public class TrainingProgramController: Controller
     {
-
+        /**
+         * CLASS: ProductTypes
+         * PURPOSE: Creates routes for main index view (buy method) and seller view (sell method)
+         * AUTHOR: Matt Kraatz
+         * METHODS:
+         *   TrainingProgramController(BangazonContext) - Constructor that saves the database context to a private variable.
+         *   IActionResult Index() - Returns a View listing all Training Programs.
+         **/
         private BangazonContext context;
 
+
+        /**
+         * Purpose: Initializes the TrainingProgramController with a reference to the database context
+         * Arguments:
+         *      ctx - Reference to the database context
+         */
         public TrainingProgramController(BangazonContext ctx)
         {
             context = ctx;
         }
 
+        /**
+         * Purpose: Creates a View list all Training Programs currently in the database
+         * Return:
+         *      View containing a list of all Training Programs
+         */
         [HttpGet]
         public IActionResult Index()
         {
