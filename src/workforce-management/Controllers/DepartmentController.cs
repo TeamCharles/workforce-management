ab63004817768813cada3d65363679b7a315ab45
Dayne Wright <daynewr@gmail.com>
2016-11-03 14:11:10 -0500

    added documentation

--- src/workforce-management/Controllers/DepartmentController.cs
+++ src/workforce-management/Controllers/DepartmentController.cs
@@ -1,21 +1,44 @@
 ﻿using BangazonWeb.Data;
 using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using workforce_management.ViewModels;
 
 namespace workforce_management.Controllers
 {
+
+    /**
+     * Class: DepartmentController
+     * Purpose: Route all department views
+     * Author: Dayne Wright
+     * Methods:
+     *     constructor DepartmentController() - returns instance of DepartmentController
+     *     view Index() - Queries for departments and returns model to razor view
+     */
+
     public class DepartmentController : Controller
     {
         private BangazonContext context;
+
+        /**
+         * Purpose: Creates instance of DepartmentController
+         * Arguments:
+         *     ctx - database context based to contructor 
+         * Return:
+         *     instance with database context
+         */
 
         public DepartmentController(BangazonContext ctx)
         {
             context = ctx;
         }
+
+        /**
+         * Purpose: Routes http get to index
+         * Arguments:
+         *     none
+         * Return:
+         *     view for razor template to index route
+         */
 
         [HttpGet]
         public IActionResult Index()
--- src/workforce-management/ViewModels/Department/DepartmentIndex.cs
+++ src/workforce-management/ViewModels/Department/DepartmentIndex.cs
@@ -1,11 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using Bangazon.Models;
 
 namespace workforce_management.ViewModels
 {
+    /**
+     * Class: DepartmentIndex
+     * Purpose: Creates viewmodel for department index route
+     * Author: Dayne Wright
+     * Methods:
+     *     constructor DepartmentIndex() - returns list of Departments
+     */
+
     public class DepartmentIndex
     {
         public IEnumerable<Department> DepartmentList { get; set; }
