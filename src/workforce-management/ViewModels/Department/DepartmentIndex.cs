6da325f40b8765fc709571faa299a182131e438d
Dayne Wright <daynewr@gmail.com>
2016-11-04 11:56:34 -0500

    updated viewmodel comment

--- src/workforce-management/ViewModels/Department/DepartmentIndex.cs
+++ src/workforce-management/ViewModels/Department/DepartmentIndex.cs
@@ -7,8 +7,6 @@ namespace workforce_management.ViewModels
      * Class: DepartmentIndex
      * Purpose: Creates viewmodel for department index route
      * Author: Dayne Wright
-     * Methods:
-     *     constructor DepartmentIndex() - returns list of Departments
      */
 
     public class DepartmentIndex
