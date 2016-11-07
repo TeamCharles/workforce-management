c9ee75f335be9c13e1a425cd83c92869b5e624b2
Matt Kraatz <Matt.Kraatz@gmail.com>
2016-11-07 13:03:13 -0800

    completed employee creation user story

--- src/workforce-management/wwwroot/js/site.js
+++ src/workforce-management/wwwroot/js/site.js
@@ -1,1 +1,15 @@
 ﻿// Write your Javascript code.
+$(document).ready(function () {
+    checkEmployeeFormStatus();
+    $("#employeeAdd input").on("change", checkEmployeeFormStatus);
+    $("#employeeAdd select").on("change", checkEmployeeFormStatus);
+
+    // Checks whether all fields on the Employee Add/Edit form have been filled in
+    function checkEmployeeFormStatus() {
+        if ($("#Employee_FirstName").val() && $("#Employee_LastName").val() && $("#Employee_StartDate").val() && $("#Employee_DepartmentId").val() && $("#Employee_ComputerId").val()) {
+            $("#employeeSubmit").attr("disabled", false);
+        } else {
+            $("#employeeSubmit").attr("disabled", true); 
+        }
+    }
+});
