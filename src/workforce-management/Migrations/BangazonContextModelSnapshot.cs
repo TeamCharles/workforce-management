09255e6c35a16b2614e7b601b8203b51a8af12ba
Anulfo <noaharkling@gmail.com>
2016-11-08 12:09:34 -0600

    migrations

---
+++ src/workforce-management/Migrations/BangazonContextModelSnapshot.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using BangazonWeb.Data;
+
+namespace workforcemanagement.Migrations
+{
+    [DbContext(typeof(BangazonContext))]
+    partial class BangazonContextModelSnapshot : ModelSnapshot
+    {
+        protected override void BuildModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.0.0-rtm-21431");
+
+            modelBuilder.Entity("Bangazon.Models.Attendee", b =>
+                {
+                    b.Property<int>("AttendeeId")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<DateTime>("DateCreated")
+                        .ValueGeneratedOnAddOrUpdate()
+                        .HasDefaultValueSql("strftime('%Y-%m-%d %H:%M:%S')");
+
+                    b.Property<int>("EmployeeId");
+
+                    b.Property<int>("ProgramId");
+
+                    b.Property<int?>("TrainingProgramId");
+
+                    b.HasKey("AttendeeId");
+
+                    b.HasIndex("EmployeeId");
+
+                    b.HasIndex("TrainingProgramId");
+
+                    b.ToTable("Attendee");
+                });
+
+            modelBuilder.Entity("Bangazon.Models.Computer", b =>
+                {
+                    b.Property<int>("ComputerId")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<DateTime>("DateCreated")
+                        .ValueGeneratedOnAddOrUpdate()
+                        .HasDefaultValueSql("strftime('%Y-%m-%d %H:%M:%S')");
+
+                    b.Property<string>("Make")
+                        .IsRequired();
+
+                    b.Property<string>("Model")
+                        .IsRequired();
+
+                    b.Property<string>("SerialNumber")
+                        .IsRequired();
+
+                    b.HasKey("ComputerId");
+
+                    b.ToTable("Computer");
+                });
+
+            modelBuilder.Entity("Bangazon.Models.Department", b =>
+                {
+                    b.Property<int>("DepartmentId")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<DateTime>("DateCreated")
+                        .ValueGeneratedOnAddOrUpdate()
+                        .HasDefaultValueSql("strftime('%Y-%m-%d %H:%M:%S')");
+
+                    b.Property<string>("Description")
+                        .IsRequired()
+                        .HasAnnotation("MaxLength", 255);
+
+                    b.Property<string>("Name")
+                        .IsRequired()
+                        .HasAnnotation("MaxLength", 50);
+
+                    b.HasKey("DepartmentId");
+
+                    b.ToTable("Department");
+                });
+
+            modelBuilder.Entity("Bangazon.Models.Employee", b =>
+                {
+                    b.Property<int>("EmployeeId")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<int>("ComputerId");
+
+                    b.Property<DateTime>("DateCreated")
+                        .ValueGeneratedOnAddOrUpdate()
+                        .HasDefaultValueSql("strftime('%Y-%m-%d %H:%M:%S')");
+
+                    b.Property<int>("DepartmentId");
+
+                    b.Property<DateTime?>("EndDate");
+
+                    b.Property<string>("FirstName")
+                        .IsRequired();
+
+                    b.Property<string>("LastName")
+                        .IsRequired();
+
+                    b.Property<DateTime>("StartDate");
+
+                    b.HasKey("EmployeeId");
+
+                    b.HasIndex("ComputerId");
+
+                    b.HasIndex("DepartmentId");
+
+                    b.ToTable("Employee");
+                });
+
+            modelBuilder.Entity("Bangazon.Models.TrainingProgram", b =>
+                {
+                    b.Property<int>("TrainingProgramId")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<DateTime>("DateCreated")
+                        .ValueGeneratedOnAddOrUpdate()
+                        .HasDefaultValueSql("strftime('%Y-%m-%d %H:%M:%S')");
+
+                    b.Property<string>("Description")
+                        .IsRequired()
+                        .HasAnnotation("MaxLength", 255);
+
+                    b.Property<string>("Name")
+                        .IsRequired()
+                        .HasAnnotation("MaxLength", 50);
+
+                    b.HasKey("TrainingProgramId");
+
+                    b.ToTable("TrainingProgram");
+                });
+
+            modelBuilder.Entity("Bangazon.Models.Attendee", b =>
+                {
+                    b.HasOne("Bangazon.Models.Employee", "Employee")
+                        .WithMany()
+                        .HasForeignKey("EmployeeId")
+                        .OnDelete(DeleteBehavior.Cascade);
+
+                    b.HasOne("Bangazon.Models.TrainingProgram", "TrainingProgram")
+                        .WithMany()
+                        .HasForeignKey("TrainingProgramId");
+                });
+
+            modelBuilder.Entity("Bangazon.Models.Employee", b =>
+                {
+                    b.HasOne("Bangazon.Models.Computer", "Computer")
+                        .WithMany()
+                        .HasForeignKey("ComputerId")
+                        .OnDelete(DeleteBehavior.Cascade);
+
+                    b.HasOne("Bangazon.Models.Department", "Department")
+                        .WithMany()
+                        .HasForeignKey("DepartmentId")
+                        .OnDelete(DeleteBehavior.Cascade);
+                });
+        }
+    }
+}
